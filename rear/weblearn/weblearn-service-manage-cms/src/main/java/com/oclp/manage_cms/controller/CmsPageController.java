b9ff82c19b6de0a2354e73bc8b1db2953d453558
Aimer <1428616906@qq.com>
2020-03-13 20:58:22 +0800

    vf

--- rear/weblearn/weblearn-service-manage-cms/src/main/java/com/oclp/manage_cms/controller/CmsPageController.java
+++ rear/weblearn/weblearn-service-manage-cms/src/main/java/com/oclp/manage_cms/controller/CmsPageController.java
@@ -3,9 +3,11 @@ package com.oclp.manage_cms.controller;
 import com.oclp.api.cms.CmsPageControllerApi;
 import com.oclp.domain.cms.CmsPage;
 import com.oclp.domain.cms.request.QueryPageRequest;
+import com.oclp.manage_cms.service.PageService;
 import com.oclp.model.response.CommonCode;
 import com.oclp.model.response.QueryResponseResult;
 import com.oclp.model.response.QueryResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +21,14 @@ import java.util.List;
 @RequestMapping("/cms/page")
 public class CmsPageController implements CmsPageControllerApi {
 
+    @Autowired
+    PageService pageService;
+
     @Override
     @GetMapping("/list/{page}/{size}")
     public QueryResponseResult findList(@PathVariable("page") int page,@PathVariable("size") int size, QueryPageRequest queryPageRequest) {
 
-        QueryResult<CmsPage> queryResult=new QueryResult<>();
+       /* QueryResult<CmsPage> queryResult=new QueryResult<>();
         List<CmsPage> list=new ArrayList<>();
         CmsPage cmsPage=new CmsPage();
         cmsPage.setPageName("测试页面");
@@ -31,6 +36,7 @@ public class CmsPageController implements CmsPageControllerApi {
         queryResult.setList(list);
         queryResult.setTotal(1);
         QueryResponseResult queryResponseResult=new QueryResponseResult(CommonCode.SUCCESS,queryResult);
-        return queryResponseResult;
+        return queryResponseResult;*/
+       return pageService.findList(page,size,queryPageRequest);
     }
 }
