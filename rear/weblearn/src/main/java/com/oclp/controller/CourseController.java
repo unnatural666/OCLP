8a2795f348ffe7e242c68099ea7eacb803c156b7
unnatural666 <1428616906@qq.com>
2020-03-22 15:11:37 +0800

    查询课程

--- rear/weblearn/src/main/java/com/oclp/controller/CourseController.java
+++ rear/weblearn/src/main/java/com/oclp/controller/CourseController.java
@@ -1,9 +1,12 @@
 package com.oclp.controller;
 
 import com.oclp.api.course.CourseControllerApi;
+import com.oclp.common.model.response.QueryResponseResult;
 import com.oclp.common.model.response.ResponseResult;
 import com.oclp.domain.course.Teachplan;
+import com.oclp.domain.course.ext.CourseInfo;
 import com.oclp.domain.course.ext.TeachplanNode;
+import com.oclp.domain.course.request.CourseListRequest;
 import com.oclp.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +28,11 @@ public class CourseController implements CourseControllerApi {
     @PostMapping("/teachplan/add")
     public ResponseResult addTeachplan(@RequestBody Teachplan teachplan) {
         return courseService.addTeachplan(teachplan);
+    }
+
+    @Override
+    @GetMapping("/coursebase/list/{page}/{size}")
+    public QueryResponseResult<CourseInfo> findCourseList(@PathVariable("page") int page,@PathVariable("size") int size, CourseListRequest courseListRequest) {
+        return courseService.findCourseList(page,size,courseListRequest);
     }
 }
--- rear/weblearn/src/main/java/com/oclp/dao/CourseMapper.java
+++ rear/weblearn/src/main/java/com/oclp/dao/CourseMapper.java
@@ -1,10 +1,16 @@
 package com.oclp.dao;
 
+import com.github.pagehelper.Page;
 import com.oclp.domain.course.CourseBase;
+import com.oclp.domain.course.ext.CourseInfo;
+import com.oclp.domain.course.request.CourseListRequest;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface CourseMapper {
 
     CourseBase findCourseBaseById(String id);
+    Page<CourseInfo> findCourseListPage(CourseListRequest courseListRequest);
 }
