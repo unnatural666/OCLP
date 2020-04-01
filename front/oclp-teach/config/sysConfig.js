9ee0c1746c9c5c7bb663f84cb1e51c587b1d166e
quanquan <767743430@qq.com>
2020-04-01 15:46:18 +0800

    s

--- front/oclp-teach/config/sysConfig.js
+++ front/oclp-teach/config/sysConfig.js
@@ -2,7 +2,7 @@ var sysConfig = {
     xcApiUrlPre: '/api',
     //xcApiUrlPre: '',
     xcApiUrl: 'http://localhost:31200',
-    imgUrl:'http://192.168.1.105/',
+    imgUrl:'http://192.168.137.84/',
     videoUrl:'http://video.xuecheng.com',
     openAuthenticate:false,
     openAuthorize:false
